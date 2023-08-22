14ebd4342c6e131af2ca9d22a0e8e6cfa17a316b
TakeshiKaga <118976367+TakeshiKaga@users.noreply.github.com>
2023-08-22 21:08:24 +0900

    associative-array.phpをアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+ 
+<head>
+    <meta charset="UTF-8">
+    <title>課題7章です</title>
+</head>
+ 
+<body>
+    <p>
+      <?php
+      // 連想配列に値を代入する
+      $food_date = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+      // 連想配列の値を出力する
+      print_r($food_date);
+      ?>
+    </p>
+</body>
+
+</html>
