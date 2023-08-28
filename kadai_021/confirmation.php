7c4b9d96e272d3032bf7300be4a95cb5718949b0
TakeshiKaga <118976367+TakeshiKaga@users.noreply.github.com>
2023-08-28 22:09:55 +0900

    confirmation.phpをアップロード

---
+++ kadai_021/confirmation.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <title>21章課題</title>
+</head>
+
+<body>
+  <h2>こちらの入力内容でお間違いありませんか？</h2>
+  <p>お名前・・・<?php echo $_POST['user_name']; ?></P>
+  <p>性別・・・<?php echo $_POST['user_gender']; ?></p>
+  <p>メールアドレス・・・<?php echo $_POST['user_email']; ?></p>
+</body>
+
+</html>
